4e9eb905ad40bd21977e1685e6720123479a214d
Pablo <contactoemail.pablo@gmail.com>
2022-07-15 15:53:09 +0200

    Banner Hover 1.0 just started, thinking about doplying it

--- components/organisms/BrandsHover.tsx
+++ components/organisms/BrandsHover.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
 const BrandsHover = () => {
   return (
-    <section>
-      <h1>BrandsHover</h1>
+    <section className="flex flex-col justify-center bg-blue-900/70 md:flex-row items-center mt-10 gap-6 px-8 max-w-[1400px] mx-auto z-0">
+      <div className="brand group">
+        <Image
+          src="/../../public/images/pixar.png"
+          layout="fill"
+          objectFit="cover"
+          width="100px"
+        />
+        <video
+          autoPlay
+          loop
+          playsInline
+          className="hidden object-cover rounded-lg group-hover:inline"
+        >
+          <source src="../../public/videos/pixar.mp4" />
+        </video>
+      </div>
     </section>
   );
 };
